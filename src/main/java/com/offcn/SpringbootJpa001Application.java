965d6d7113c044b99fb4efc693e8f0a1c1b6d514
ls1940103583 <59644115+ls1940103583@users.noreply.github.com>
2020-01-08 19:56:54 +0800

    Update SpringbootJpa001Application.java

--- src/main/java/com/offcn/SpringbootJpa001Application.java
+++ src/main/java/com/offcn/SpringbootJpa001Application.java
@@ -2,7 +2,7 @@ package com.offcn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//********************网站修改************************
 @SpringBootApplication
 public class SpringbootJpa001Application {
     /**
